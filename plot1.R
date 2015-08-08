86d90abd6c7e720aed6314da227e3ef558c9f128
Jose A Valdez Crespo <javcsjc@gmail.com>
2015-08-07 19:12:58 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -11,7 +11,8 @@
 ## For each plot you should
 ## 1.- Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
 ## 2.- Name each of the plot files as plot1.png, plot2.png, etc.
-## 3.- Create a separate R code file (plot1.R, plot2.R, etc.) that constructs the corresponding plot, i.e. code in plot1.R constructs the plot1.png plot. 
+## 3.- Create a separate R code file (plot1.R, plot2.R, etc.) that constructs the corresponding plot.
+##     i.e. code in plot1.R constructs the plot1.png plot. 
 ##     Your code file should include code for reading the data so that the plot can be fully reproduced.
 ##     You must also include the code that creates the PNG file.
 ## 4.- Add the PNG file and R code file to the top-level folder of your git repository (no need for separate sub-folders)
